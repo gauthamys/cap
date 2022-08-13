1f97b811ecc976ff58e7d7dc98351f92bcfbd43a
gauthamys <gauthamys123@gmail.com>
2022-08-13 11:40:21 +0530

    essay page

--- pages/essay.js
+++ pages/essay.js
@@ -1,8 +1,20 @@
 import React from "react";
+import styles from "../styles/Essay.module.css";
 import commonStyles from "../styles/Common.module.css";
 
 export default function Essay(props){
     return(
-        <h1 className={commonStyles.title}>Essay</h1>
+        <div className={commonStyles.centeredDiv}>
+            <div className={commonStyles.pageHead}>
+                <p className={commonStyles.title}>Essay Assessment</p>
+            </div>
+            <div className={styles.essayInput}>
+                <div class={styles.prompt}>
+                    <p>Prompt</p>
+                    <button href='/essay' className={styles.submitEssay}>Submit to see score and feedback</button>
+                </div>
+                <textarea className={styles.textarea} placeholder="Write your essay here..."></textarea>
+            </div>
+        </div>
     )
 }
