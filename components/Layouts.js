ad1f0b1084d0b2f7cdaa09c9aba6d25c5f5b67aa
gauthamys <gauthamys123@gmail.com>
2022-08-14 20:08:28 +0530

    wp flex

--- components/Layouts.js
+++ components/Layouts.js
@@ -11,6 +11,7 @@ const Layout = ({ children }) => {
     <HomeButton />
   </main>
   <div className={styles.footer}>
+    <p>PW_AMJ_04</p>
   {/* <p>&copy; {new Date().getFullYear()}</p> */}
   {/* <Image src="/pes.png" alt="Next.js" height={50} width={50} /> */}
   </div>
