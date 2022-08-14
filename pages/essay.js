8aac7d55e13eca80b9bf3fef094df9117cef14f9
gauthamys <gauthamys123@gmail.com>
2022-08-14 19:46:18 +0530

    wp

--- pages/essay.js
+++ pages/essay.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Essay.module.css";
 import commonStyles from "../styles/Common.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,13 +7,30 @@ import axios from "axios";
 
 export default function Essay(props){
     const [prompts, setPrompts] = useState([]);
-    
+    const [prompt, setPrompt] = useState("");
+    const [essay, setEssay] = useState('');
+
     useEffect(() => {
         // fetch prompts from the database
         setPrompts([
             'prompt1', 'prompt2', 'prompt3', 'prompt4', 'prompt5', 'prompt6', 'prompt7', 'prompt8', 'prompt9', 'prompt10'
         ])
     }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            prompt: prompt,
+            essay: essay
+        }
+        console.log(data);
+        // axios.post('/api/essay', data)
+        //     .then(res => {
+        //         console.log(res)
+        //     }).catch(err => {
+        //         console.log(err)
+        //     })
+    }
 
     return(
         <div className={commonStyles.centeredDiv}>
@@ -23,18 +40,18 @@ export default function Essay(props){
             <div className={styles.essayInput}>
                 <div className={styles.prompt}>
                     <p>Prompt</p>
-                    <select className={styles.select}>
+                    <select className={styles.select} onChange={(e) => setPrompt(e.target.value)} value={prompt}>
                         {
                             prompts.map((prompt, index) => {
                                 return <option key={index}>{prompt}</option>
                             })
                         }
                     </select>
-                    <button href='/essay' className={styles.submitEssay}>
+                    <button href='/essay' className={styles.submitEssay} onClick={handleSubmit}>
                         <FontAwesomeIcon icon={faPaperPlane} />
                     </button>
                 </div>
-                <textarea className={styles.textarea} placeholder="Write your essay here..."></textarea>
+                <textarea value={essay} onChange={(e) => setEssay(e.target.value)} className={styles.textarea} placeholder="Write your essay here..."></textarea>
             </div>
         </div>
     )
--- pages/word-problems.js
+++ pages/word-problems.js
@@ -1,15 +1,44 @@
 import React from "react";
 import commonStyles from "../styles/Common.module.css";
 import styles from "../styles/WordProblems.module.css";
+import flask from '../public/flask1.svg';
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFlask } from "@fortawesome/free-solid-svg-icons";
 
 export default function WordProblems(props){
+    const [problem, setProblem] = React.useState('');
+    const [solve, setSolve] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+    function handleProblemChange(e) {
+        setProblem(e.target.value);
+    }
+    function handleClick(e){
+        //setSolve(!solve);
+        setLoading(true);
+        // fetch solution from server and display it
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }
     return(
         <div className={commonStyles.centeredDiv}>
             <div className={commonStyles.pageHead}>
                 <p className={commonStyles.title}>Word Problem Solver</p>
             </div>
-            <div className={styles.wordProblemInput}>
-
+            <div className={styles.container}>
+                <div className={styles.questionContainer}>
+                    <p>Enter word problem here</p>
+                    <textarea placeholder="> " value={problem} className={styles.wpInput} onChange={handleProblemChange}>
+                    </textarea>
+                    <a className={styles.submit} onClick={handleClick}>
+                        <FontAwesomeIcon icon={faFlask} /> solve
+                    </a>
+                </div>
+                <div className={styles.solution}>
+                    <img src='/flask.svg' className={loading ? styles.bgImage : styles.bgImageHidden}/>
+                    <p>Solution</p>
+                </div>
             </div>
         </div>    
     )
